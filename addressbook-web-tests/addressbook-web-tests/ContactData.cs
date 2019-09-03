388ec993b132aaeac90692e3c16cdfb44a54eebd
Liza G <gurovaliz@gmail.com>
2019-09-03 12:13:15 +0300

    Задание 4. Создание теста ContactCreationTest

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class ContactData
+    {
+        private string name;
+        private string surname = "";
+        public ContactData(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+    }
+
+  
+}
