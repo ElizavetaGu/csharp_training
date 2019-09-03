15e6328c6675cedffc27201d6cc8c411d7b47557
Liza G <gurovaliz@gmail.com>
2019-09-03 19:29:27 +0300

    Задание 5. Создание TestBase

--- addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupRemovalTest.cs
@@ -8,24 +8,21 @@ using NUnit.Framework;
 namespace addressbook_web_tests
 {
     [TestFixture]
-    public class GroupCreationTests : TestBase
+    public class GroupRemovalTests : TestBase
     {
-       
+ 
         [Test]
-        public void GroupCreaationTest()
+        public void GroupRemovalTest()
         {
             OpenHomePage();
             Login(new AccountData("admin", "secret"));
             GoToGroupsPage();
-            InitGroupCreation();
-            GroupData group = new GroupData("name");
-            group.Header = "fff";
-            group.Footer = "aaa";
-            FillGroupForm(group);
-            SubmitGroupCreation();
+            SelectGroup();
+            RemoveGroup();
             ReturnToGroupsPage();
             Logout();
         }
 
     }
 }
+
