7270e45a820dfec619052b3e5581ccffec52d9e2
Liza G <gurovaliz@gmail.com>
2019-09-29 16:42:16 +0300

    Задание 11, тест проверки соответствия инфы на главной странице инфе в форме редактирования

---
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactInfoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_web_tests
+{
+    [TestFixture]
+    public class ContactInfoTests : AuthTestBase
+    {
+
+        [Test]
+        public void ContactInfoEditPageTest()
+        {
+            ContactData fromTable = app.Contacts.GetContactInfoFromTable(0);
+            ContactData fromForm = app.Contacts.GetContactInfoFromEditForm(0);
+
+            Assert.AreEqual(fromTable, fromForm);
+            Assert.AreEqual(fromTable.Address, fromForm.Address);
+            Assert.AreEqual(fromTable.AllPhones, fromForm.AllPhones);
+        }
+
+    }
+}
--- addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/addressbook-web-tests/model/ContactData.cs
@@ -3,20 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace addressbook_web_tests
 {
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
+        public string allPhones;
+        public string allEmails;
+
         public ContactData(string name)
         {
             Name = name;
         }
 
         public string Name { get; set; }
-
         public string Surname { get; set; }
+        public string Address { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string Email1 { get; set; }
+        public string Email2 { get; set; }
+        public string Email3 { get; set; }
+        public string AllPhones
+        {
+            get
+            {
+                if (allPhones != null)
+                {
+                    return allPhones;
+                }
+                else
+                {
+                    return (CleanUp(HomePhone) + CleanUp(MobilePhone) + CleanUp(WorkPhone)).Trim();
+                }
+            }
+            set
+            {
+                allPhones = value;
+            }
+        }
+        public string AllEmails
+        {
+            get
+            {
+                if (allEmails != null)
+                {
+                    return allEmails;
+                }
+                else
+                {
+                    return (Email1 + Email2 + Email3).Trim();
+                }
+            }
+            set
+            {
+                allEmails = value;
+            }
+        }
 
+        private string CleanUp(string phone)
+        {
+            if (phone == null || phone == "")
+            {
+                return "";
+            }
+            return Regex.Replace(phone, "[ -()]", "") + "\r\n";
+        }
+         
         public bool Equals(ContactData other)
         {
             if (Object.ReferenceEquals(other, null))
